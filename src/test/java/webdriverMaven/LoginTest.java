4345a49e0d6e63d153ce09d55f6a899a90c1fe60
Sushil <sushilpatial99@gmail.com>
2020-10-06 23:22:54 +0530

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,41 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		
+		WebDriverManager.chromedriver().setup(); 
+		driver = new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin() {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("trainse@gmail.com");
+	
+		
+		
+	}
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+		
+	}
+	
+
+}
